34fcb7d1605bb355201117a341def0fd79b0d980
Galante <fabiogalantemans@gmail.com>
2019-12-21 05:13:10 -0300

    Add project files.

---
+++ Confitec.Usuarios.API/Models/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confitec.Usuarios.API.Models
+{
+    public class Usuarios
+    {
+        
+    }
+}
